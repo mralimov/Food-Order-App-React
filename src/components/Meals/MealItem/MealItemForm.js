ca1d0c31e1c3efb314a50d0afa6f64fe5dfdcf81
mralimov <mralimov89@gmail.com>
2021-06-09 10:52:37 -0400

    Meal Item Component

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,11 @@
+import styling from './MealItemForm.module.css';
+
+const MealItemForm = props => {
+  return (
+    <form className={styling.form}>
+      <div></div>
+    </form>
+  );
+};
+
+export default MealItemForm;
