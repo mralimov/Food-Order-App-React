b641f538e66f2d2924625cea5e6da35b23d720ab
mralimov <mralimov89@gmail.com>
2021-06-09 01:12:35 -0400

    Header component update

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,13 @@
 import { Fragment } from 'react';
 import mealsImg from '../../assets/meals.jpg';
 import headerStyle from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 const Header = props => {
   return (
     <Fragment>
       <header className={headerStyle.header}>
         <h1>Food Order App react</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={headerStyle['main-image']}>
         <img src={mealsImg} alt="A table full of delicious food!" />
