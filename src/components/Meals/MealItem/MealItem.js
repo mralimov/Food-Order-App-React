468239c46de1776352a5a70010fb2a17a77ca70b
mralimov <mralimov89@gmail.com>
2021-06-09 10:53:11 -0400

    Meal Item Component

---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,17 @@
+import styling from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
+const MealItem = props => {
+  const price = `$${props.price.toFixed(2)}`;
+  return (
+    <li className={styling.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={styling.description}>{props.description}</div>
+        <div className={styling.price}>{price}</div>
+      </div>
+      <MealItemForm></MealItemForm>
+    </li>
+  );
+};
+
+export default MealItem;
