7f4791f5de3df86f921c131f89b02f2829df3539
mralimov <mralimov89@gmail.com>
2021-06-09 14:26:30 -0400

    importing CartProvider to App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import { Fragment } from 'react';
+import { useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './components/store/CartProvider';
 function App() {
+  const [seeCart, setSeeCart] = useState(false);
+
+  const showCartHandler = () => {
+    setSeeCart(true);
+  };
+
+  const hideCartHandler = () => {
+    setSeeCart(false);
+  };
   return (
-    <Fragment>
-      <Header />
+    <CartProvider>
+      {seeCart && <Cart onClose={hideCartHandler} />}
+      <Header onSeeCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
