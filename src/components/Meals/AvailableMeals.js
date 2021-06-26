26d0fc4d1e313fdd4a85e8440806eb374c389f92
mralimov <mralimov89@gmail.com>
2021-06-26 17:24:52 -0400

    debugging

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -29,20 +29,15 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map(
-    meal => (
-      console.log(meal.price),
-      (
-        <MealItem
-          id={meal.id}
-          key={meal.id}
-          name={meal.name}
-          description={meal.description}
-          price={meal.price}
-        />
-      )
-    )
-  );
+  const mealsList = DUMMY_MEALS.map(meal => (
+    <MealItem
+      id={meal.id}
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
   return (
     <section className={styling.meals}>
       <Card>
