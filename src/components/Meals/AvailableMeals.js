25e7dbfb49dbeb94bf7cf605ed3056cace2a9d0e
mralimov <mralimov89@gmail.com>
2021-06-27 22:00:24 -0400

    Loading state

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
@@ -22,10 +23,17 @@ const AvailableMeals = () => {
         });
       }
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
     fetchMeals();
   }, []);
-
+  if (isLoading) {
+    return (
+      <section className={styling.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
   const mealsList = meals.map(meal => (
     <MealItem
       id={meal.id}
